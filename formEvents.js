b87000ae7cb0e4269a6d5ad28a069f174485e828
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-05-07 17:07:12 -0500

    first commit

---
+++ formEvents.js
@@ -0,0 +1,12 @@
+const formEvent = document.querySelector('#shelterForm');
+const input = document.querySelector('#catName');
+const list = document.querySelector('#lists');
+formEvent.addEventListener('submit', function(e) {
+    e.preventDefault();
+    /*console.log("Hurray! Submitted!")*/
+    const name = input.value;
+    const LIs = document.createElement('li');
+    LIs.innerText = name;
+    list.append(LIs);
+    input.value = '';
+})
